2379c2dbb528bfbacc82ca5e22d920cdc32e8b8e
suxiaocheng <suxiaocheng2010@hotmail.com>
2022-08-21 15:17:54 +0800

    build script: update build script to use cpu dynamic

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NUM_CPU=`cat /proc/cpuinfo |grep processor | wc -l`
+NUM_CPU=$((NUM_CPU*2))
+
 pushd ../linux
 KERNEL=kernel8
 if [ ! -f .config ]; then
@@ -8,6 +11,6 @@ fi
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
 cp defconfig arch/arm64/configs/bcm2711_defconfig
 
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs -j16
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs -j${NUM_CPU}
 
 popd
